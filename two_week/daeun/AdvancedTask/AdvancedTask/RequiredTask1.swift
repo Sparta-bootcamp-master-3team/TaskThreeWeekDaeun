8df20aa4e1484ebf4bdde290693a1e7a721bfab7
KwakDADA <down022@gmail.com>
2025-03-18 11:55:39 +0900

    refactor: 필수 문제 1 파일 분리

---
+++ two_week/daeun/AdvancedTask/AdvancedTask/RequiredTask1.swift
@@ -0,0 +1,18 @@
+//
+//  RequiredTask1.swift
+//  AdvancedTask
+//
+//  Created by 곽다은 on 3/18/25.
+//
+
+
+let sum: (Int, Int) -> String = { "두 수의 합은 \($0 + $1)입니다." }
+
+func calculate(_ operate: (Int, Int) -> String) {
+    print(operate(1, 2))
+}
+
+func testRequiredTask1() {
+    print("-- Required Task 1 --")
+    calculate(sum)
+}
--- two_week/daeun/AdvancedTask/AdvancedTask/main.swift
+++ two_week/daeun/AdvancedTask/AdvancedTask/main.swift
@@ -6,13 +6,7 @@
 //
 
 // 필수 문제 1
-let sum: (Int, Int) -> String = { "두 수의 합은 \($0 + $1)입니다." }
-
-func calculate(_ operate: (Int, Int) -> String) {
-    print(operate(1, 2))
-}
-
-calculate(sum)
+testRequiredTask1()
 
 
 // 필수 문제 2
